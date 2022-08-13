38a1fc06bf474aaa1d663a1b881c488187637b78
Barry Wallis <barry.wallis@outlook.com>
2022-08-13 15:17:45 -0700

    Chapter 7: Added CommandContext

---
+++ CommandAPI/Data/CommandContext.cs
@@ -0,0 +1,14 @@
+﻿using CommandAPI.Models;
+
+using Microsoft.EntityFrameworkCore;
+
+namespace CommandAPI.Data;
+
+public class CommandContext : DbContext
+{
+    public required DbSet<Command> CommandItems { get; set; }
+
+    public CommandContext(DbContextOptions<CommandContext> options) : base(options)
+    {
+    }
+}
