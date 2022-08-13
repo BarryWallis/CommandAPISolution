068627ad08213d397702d8dea21c0b9ead546eab
Barry Wallis <barry.wallis@outlook.com>
2022-08-12 20:03:59 -0700

    Chapter 6: Added Command Model

---
+++ CommandAPI/Models/Command.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CommandAPI.Models;
+
+public record Command()
+{
+    [Key]
+    [Required]
+    public int Id { get; init; }
+
+    [Required]
+    [MaxLength(250)]
+    public string HowTo { getl init; }
+
+    [Required]
+    public string Platform { get; init; }
+
+    [Required]
+    public string HowTo { get; init; }
+}
